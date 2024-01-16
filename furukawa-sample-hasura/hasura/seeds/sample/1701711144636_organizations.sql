964c6ccaaf3a68effa80b86ca5d4334783bca4e1
isao-furukawa <isao.furukawa.0315@gmail.com>
2024-01-16 11:21:14 +0900

    seed追加

---
+++ furukawa-sample-hasura/hasura/seeds/sample/1701711144636_organizations.sql
@@ -0,0 +1,5 @@
+SET check_function_bodies = false;
+INSERT INTO public.organizations (id, organization_name, created_at, updated_at) VALUES (1, '山本果物株式会社', '2024-01-15T11:20:15.201072+09:00', '2024-01-15T11:20:15.201072+09:00');
+INSERT INTO public.organizations (id, organization_name, created_at, updated_at) VALUES (2, 'Tropical Delight Fruits, Inc.', '2024-01-15T11:20:24.013534+09:00', '2024-01-15T11:20:24.013534+09:00');
+INSERT INTO public.organizations (id, organization_name, created_at, updated_at) VALUES (3, 'VietFruit Harvest Co.', '2024-01-15T11:23:52.383517+09:00', '2024-01-15T12:30:56.32861+09:00');
+INSERT INTO public.organizations (id, organization_name, created_at, updated_at) VALUES (4, 'SKYDISC', '2024-01-15T12:38:38.998381+09:00', '2024-01-15T12:38:38.998381+09:00');
